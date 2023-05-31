0426d76d4b01a0cef0e0d4d70ac8cbea25bfb343
diego <diegonegocios21@outlook.com>
2023-05-31 15:36:14 -0300

    Complete

--- src/scripts/modal.js
+++ src/scripts/modal.js
@@ -1,1 +1,27 @@
 /* Desenvolva seu código aqui... */
+function showModal() {
+    const btnCadastrar = document.querySelector('.btn-cadastrar');
+    const modalContainer = document.querySelector('#modalContainer');
+    const modalBackground = document.querySelector('.modal-background');
+
+  
+    btnCadastrar.addEventListener('click', () => {
+        modalContainer.showModal();
+        modalBackground.classList.add('modal-background--show');
+        closeModal();
+    });
+}
+
+function closeModal() {
+    const closeModal = document.querySelector('.modal__input-button');
+    const modalContainer = document.querySelector('#modalContainer');
+    const modalBackground = document.querySelector('.modal-background');
+  
+    closeModal.addEventListener('click', () => {
+        modalContainer.close();
+        modalBackground.classList.remove('modal-background--show');
+    });
+}
+
+showModal();
+
